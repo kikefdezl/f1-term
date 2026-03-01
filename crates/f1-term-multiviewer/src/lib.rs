841ff95526e465bb8376df0891e92674aa641386
kike <enriquelagui@outlook.com>
2026-03-01 16:13:01 +0100

    refactor: declare full response DTOs for multiviewer endpoint

--- crates/f1-term-multiviewer/src/lib.rs
+++ crates/f1-term-multiviewer/src/lib.rs
@@ -24,9 +24,60 @@ impl Default for MultiviewerClient {
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[allow(dead_code)]
+pub struct TrackPosition {
+    pub x: f64,
+    pub y: f64,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[allow(dead_code)]
+pub struct CircuitFeature {
+    pub angle: f64,
+    pub length: f64,
+    pub number: i32,
+    pub track_position: TrackPosition,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[allow(dead_code)]
+pub struct CandidateLap {
+    pub driver_number: String,
+    pub lap_number: i32,
+    pub lap_start_date: String,
+    pub lap_start_session_time: f64,
+    pub lap_time: f64,
+    pub session: String,
+    pub session_start_time: f64,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[allow(dead_code)]
 struct MultiviewerCircuitResponse {
-    x: Vec<i32>,
-    y: Vec<i32>,
+    pub candidate_lap: Option<CandidateLap>,
+    pub circuit_key: u32,
+    pub circuit_name: String,
+    pub corners: Vec<CircuitFeature>,
+    pub country_ioc_code: String,
+    pub country_key: u32,
+    pub country_name: String,
+    pub location: String,
+    pub marshal_lights: Vec<CircuitFeature>,
+    pub marshal_sectors: Vec<CircuitFeature>,
+    pub meeting_key: String,
+    pub meeting_name: String,
+    pub meeting_official_name: Option<String>,
+    pub mini_sectors_indexes: Vec<usize>,
+    pub race_date: String,
+    pub rotation: f64,
+    pub round: u32,
+    pub x: Vec<i32>,
+    pub y: Vec<i32>,
+    pub year: u32,
 }
 
 impl CircuitLayoutProvider for MultiviewerClient {
